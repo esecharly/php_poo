a7594d8e52c57ff54ed9238cf614c1a91778ed27
esecharly <62493210+esecharly@users.noreply.github.com>
2021-03-25 18:15:59 -0600

    Initial commit

---
+++ clases.php
@@ -0,0 +1,13 @@
+<?php
+    //una clase es el molde de trabajo para nuestros metodos y atributos
+
+    class Miclase{
+        //definif atributos
+
+        //definir metodos
+    }
+
+    class Clase2{
+
+    }
+?>
